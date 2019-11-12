f2781f71b436238fa8639c4eec0b42c5cf55bf38
Shoaib Khan <mshoaibkhan91@gmail.com>
2019-11-12 21:47:01 +0100

    added utilization read in chunks

--- analysis/ReadUtilizationFile.py
+++ analysis/ReadUtilizationFile.py
@@ -1,7 +1,9 @@
 import numpy as np
 from app.logging import CSVLogger
 
-path_to_csv = "data/volume_tick.csv"
+#path_to_csv = "data/volume_tick.csv"
+path_to_csv = "data/capacity1_3000cars_800tiks.csv"
+path_to_write = "volume"
 #data = np.genfromtxt(path_to_csv, dtype=int, delimiter=',', names=False)
 
 edges = []
@@ -14,7 +16,20 @@ for rows in range(len(utilfiles)):
     floatData.append(floatRow)
 
 npData = np.array(floatData)
-sumAlongCols = npData.sum(axis=0)
 
-CSVLogger.logEvent("volume", [edges])
-CSVLogger.logEvent("volume", [sum for sum in sumAlongCols])
+def aggregateAll():
+    """Aggregate all the volume data by summing along the columns"""
+    sumAlongCols = npData.sum(axis=0)
+    CSVLogger.logEvent(path_to_write, [edges])
+    CSVLogger.logEvent(path_to_write, [sum for sum in sumAlongCols])
+
+
+def aggregateChunks(num):
+    """sum up the chunks of utilizations in capacity or volume files"""
+    sumChunks = np.add.reduceat(npData, range(0, len(npData), num), axis=0)
+    CSVLogger.logEvent(path_to_write, [edges])
+    for i in range(len(sumChunks)):
+        CSVLogger.logEvent(path_to_write, [sum for sum in sumChunks[i]])
+
+
+aggregateChunks(100)
